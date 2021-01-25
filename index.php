8df5d337205e9a1526ef9d6aaf31b5378c0939c2
altair D. souza <altai.kof@gmail.com>
2021-01-25 14:11:36 -0300

    treinando o css grid (lastnames.myheritage.com.br/last-name/Sdsadsa)

---
+++ index.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="lib/css/style.css">
+    <title>MyHeritage</title>
+</head>
+<body>
+<div class="container">
+<header>
+    <nav>
+        <ul>
+            <li>A sua sessão não foi iniciada</li>
+            <li>iniciar sessão</li>
+            <li>Cadastre-se</li>
+            <li>Ir Premium</li>
+            <li>Ajuda</li>
+            <li>Portguês-Brasil</li>
+        </ul>
+    </nav>
+    <div class="img_logo">
+        <img src="lib/img/logo.png">
+    </div>
+</header>
+
+
+<main>
+    <div class="title">
+        <h1>Explore o Nome da Família Ríbeiro</h1>
+    </div>
+    <div class="naruto">
+        <h2>Descubra a história por trás do sobrenome Sdsadsa</h2>
+
+        <p>Registros históricos oferecem uma janela incomum para o passado
+        da sua família. Saiba mais sobre a família Sdsadsa, onde eles
+        viveram e o que eles fizeram para ganhar a vida. Pesquise por
+        registros de nascimento, certidões de óbito e dados de
+        imigração, entre outras informações em coleções históricas. Encontre os
+        nomes e datas de nascimento de membros da família Sdsadsa, os diferentes
+        endereços de residência, as
+        ocupações dos membros da família bem como informações funerárias e da morte.</p>
+        <br>
+        <h2>Nós encontramos 22 registros para o sobrenome Sdsadsa</h2>
+        <nav>
+            <ul>
+                <li>21 Árvores</li>
+                <li>0 Nascimento, Casamento e Morte</li>
+                    <li>0 fotos</li>
+                <li>0 Lista de Censos e Eleitor</li>
+                <li>Ver todos os registros</li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="sasuke">
+        <div class="subt">
+            <h2>Distribuição de registros para Família Ríbeiro</h2>
+        </div>
+        <div class="map">
+            <img src="lib/img/map.png">
+        </div>
+        <div class="subtt">
+            <h2>Procure em nossas coleções de registros</h2>
+        </div>
+        <div class="lorem">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet
+                aut delectus esse eveniet impedit, in inventore ipsa iusto labore
+                maxime, nemo, officia. Alias error, iste minima molestias quaerat sed.
+            </p>
+        </div>
+    </div>
+
+    <div class="sakura">
+
+       <h3> Mais informação sobre a família Sdsadsa está disponível no MyHeritage</h3>
+        <p>A melhor forma para rastrear e organizar sua pesquisa é construindo uma árvore genealógica.
+            Começar sua árvore genealógica é rápido, fácil e grátis. Adicione suas<br> informações básicas para iniciar.</p>
+
+    <form>
+
+        <select id="genero">
+            <option value="">Selecione</option>
+            <option value="">masculino</option>
+            <option value="">feminimo</option>
+    </select>
+
+        <input type="text" id="nome" name="nome" placeholder="Nome">
+
+        <input type="text" id="sobrenome" name="sobrenome" placeholder="Sobrenome">
+
+        <input type="text" id="E-mail" name="E-mail" placeholder="E-mail">
+
+        <input type="date" id="data" name="data">
+    </form>
+        <p>Ao Cadastrar-Se, Você Concorda Com Os Termos De Serviço E A Política De Privacidade.</p>
+        <button type="submit">Iniciar</button>
+    </div>
+
+
+</main>
+
+<footer>
+
+    <div class="footer1">
+        <img src="lib/img/MyHeritage.png">
+        <strong >Ir Premium</strong>
+        <h4>Encomendar kits de DNA</h4>
+        <ul>
+            <li><img src="lib/img/facebook.png"></li>
+            <li> <img src="lib/img/twitter.png"></li>
+            <li><p>Direitos autorais © 2021 MyHeritage Ltd.</p></li>
+        </ul>
+
+
+
+    </div>
+    <div class="footer2">
+
+        <p>Home</p>
+        Cadastre-se gratuitamente<br>
+        DNA<br>
+        Árvore<br>
+        Registros históricos<br>
+        Colora fotos<br>
+        Realce fotos Novo<br>
+        Family Tree Builder<br>
+    </div>
+    <div class="footer3">
+        <p>Sobre nós</p>
+        Blog<br>
+        Histórias de usuários<br>
+        Nosso time<br>
+    </div>
+    <div class="footer4">
+        <p>Suporte</p>
+        Entre em contato conosco<br>
+        Política de privacidade Atualizado<br>
+        Termos de Serviço<br>
+        Informação sobre Cookie<br>
+        Lista de preços<br>
+        Portal de Conhecimento<br>
+    </div>
+</footer>
+</body>
+</div>
+</html>
